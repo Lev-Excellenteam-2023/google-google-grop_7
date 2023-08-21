8ba4f8bb937a7eca724a5241b792d16b47dea21f
yaniv051293 <yaniv051293@gmail.com>
2023-08-21 15:54:04 +0300

    finish main

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import insert_data
 import search_mode
 import read_files
+import re
 
 
 def main():
     read_files.read_files_in_directory()
     user_input = ""
-    while not (user_input.endswith('#')):
+    running = True
+    while running:
         print("Enter a search")
         if user_input != "":
             print(user_input, end="")
         user_input += input()
-        list_input = user_input.split()
-        user_input = ""
-        for word in list_input:
-            word = insert_data.remove_non_alnum(word)
-
-
-        user_input = insert_data.remove_non_alnum(user_input)
+        if user_input.endswith('#'):
+            running = False
+        user_input = re.sub(r'[^a-zA-Z0-9]', ' ', user_input)
+        user_input = re.sub(r'\s+', ' ', user_input).strip()
 
         all_result = search_mode.search_engine(user_input, insert_data.words_dict)
         result = search_mode.extract_5_members(insert_data.line_dict, all_result)
